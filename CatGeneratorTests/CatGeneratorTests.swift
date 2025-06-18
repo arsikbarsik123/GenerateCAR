d4504ef8f35587f514da25d73ef7e09d0ccf5834
Arsalan <batyavzdanii749@gmail.com>
2025-06-18 16:09:42 +0300

    Initial Commit

---
+++ CatGeneratorTests/CatGeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatGeneratorTests.swift
+//  CatGeneratorTests
+//
+//  Created by Ширапов Арсалан on 18.06.2025.
+//
+
+import Testing
+@testable import CatGenerator
+
+struct CatGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
